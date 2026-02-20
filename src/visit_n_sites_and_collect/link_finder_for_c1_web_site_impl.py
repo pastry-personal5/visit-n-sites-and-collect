0b528502440e43df78860071258e7904efb1dd8b
Zhang-hoon Dennis Oh <zhanghoondennisoh@gmail.com>
2026-02-20 22:58:58 +0900

    refactor(link-finder): improve network request robustness
    Introduce timeouts for `requests.get` calls to prevent indefinite hanging.
    Expand error handling to catch `Timeout`, `HTTPError`, and general `RequestException` for more resilient network operations.
    Refactor the `find_campaign_links` method into a private `_find_campaign_links` and extract `_get_campaign_links_from_article` to improve modularity and clarity.

--- src/visit_n_sites_and_collect/link_finder_for_c1_web_site_impl.py
+++ src/visit_n_sites_and_collect/link_finder_for_c1_web_site_impl.py
@@ -1,5 +1,5 @@
+from typing import List
 from urllib.parse import urljoin
-import time
 
 from bs4 import BeautifulSoup
 from loguru import logger
@@ -7,10 +7,14 @@ import requests
 
 from src.visit_n_sites_and_collect.link_finder_impl_base import LinkFinderImplBase
 from src.visit_n_sites_and_collect.publisher import PublisherController
-from src.visit_n_sites_and_collect.link_finder_filter import LinkFinderFilter
+from src.visit_n_sites_and_collect.article_link_to_campaign_link_cache import ArticleLinkToCampaignLinkCache
 
 
 class LinkFinderForC1WebSiteImpl(LinkFinderImplBase):
+
+    def __init__(
+        self, article_link_to_campaign_link_cache: ArticleLinkToCampaignLinkCache):
+        super().__init__(article_link_to_campaign_link_cache)
 
     def get_publisher_meta(self):
         target_base_url_list = [
@@ -31,25 +35,33 @@ class LinkFinderForC1WebSiteImpl(LinkFinderImplBase):
         publisher_controller = PublisherController()
         publisher_links_to_visit = publisher_controller.create_publisher_links_to_visit(target_base_url_list, template_list, days_difference_since_last_run)
         for publisher_link in publisher_links_to_visit:
-            campaign_links = self.find_campaign_links(publisher_link)
+            campaign_links = self._find_campaign_links(publisher_link)
             set_of_campaign_links.update(campaign_links)
         logger.info(f"Finally, got ({len(set_of_campaign_links)}) of campaign links.")
         return set_of_campaign_links
 
-    def find_campaign_links(self, publisher_link: str):
-        # Initialize a list to store campaign links
+    def _find_campaign_links(self, publisher_link: str):
         campaign_links = []
 
-        logger.info(f"Visiting ({publisher_link})...")
+        logger.info(f"Visiting {publisher_link} ...")
 
-        # Send a request to the |publisher_link|
         try:
-            response = requests.get(publisher_link)
+            const_timeout_in_seconds = 16
+            response = requests.get(publisher_link, timeout=const_timeout_in_seconds)
+            response.raise_for_status()
         except requests.exceptions.ConnectionError:
+            return []
+        except requests.exceptions.Timeout:
+            logger.warning(f"Timeout when getting the link: {publisher_link}")
+            return []
+        except requests.exceptions.HTTPError as http_err:
+            logger.warning(f"HTTP error occurred when getting the link: {publisher_link} - {http_err}")
+            return []
+        except requests.exceptions.RequestException as req_err:
+            logger.warning(f"Request exception occurred when getting the link: {publisher_link} - {req_err}")
             return []
         soup = BeautifulSoup(response.text, "html.parser")
 
-        # Find all <span> elements with class 'list_subject' and get 'a' tags
         list_of_article_elements = soup.find_all("span", class_="list_subject")
         partial_article_links = []
         for span in list_of_article_elements:
@@ -57,39 +69,54 @@ class LinkFinderForC1WebSiteImpl(LinkFinderImplBase):
             if a_tag and "네이버" in a_tag.text:
                 partial_article_links.append(a_tag["href"])
 
-        # Check each Naver link
         for partial_article_link in partial_article_links:
             article_link = urljoin(publisher_link, partial_article_link)
 
-            # It looks up an entry in the cache, first.
-            campaign_links = self.article_link_to_campaign_link_cache.get(article_link)
-            if campaign_links:
-                logger.info(f"Cache hit. Now returning entries from the cache. ({article_link})")
-                return campaign_links
-
-            logger.info(f"Visiting ({article_link})...")
-            campaign_links = []
-            try:
-                res = requests.get(article_link)
-            except requests.exceptions.ConnectionError:
-                logger.warning(f"Could not get the link: ({article_link}). Continue...")
+            cached_links = self.article_link_to_campaign_link_cache.get(article_link)
+            if cached_links is not None:
+                logger.info(f"Cache hit. Using cached entries. ({article_link})")
+                campaign_links.extend(cached_links)
                 continue
-            inner_soup = BeautifulSoup(res.text, "html.parser")
 
-            # Find all links that start with the campaign URL
-            for a_tag in inner_soup.find_all("a", href=True):
-                campaign_link = a_tag["href"]
-                if self.is_starting_with_campaign_url(campaign_link):
-                    campaign_links.append(campaign_link)
-                else:
-                    # Log only significant links.
-                    # We don't want to log all links, because there are too many of them.
-                    if campaign_link.find("http") != -1 and campaign_link.find("clien") == -1:
-                        logger.info(f"Not a campaign link: {campaign_link}")
+            article_campaign_links = self._get_campaign_links_from_article(article_link)
 
-            # Note: The length of |campaign_links| can be zero. It's intentional.
-            self.article_link_to_campaign_link_cache.put(article_link, campaign_links)
+            self.article_link_to_campaign_link_cache.put(article_link, article_campaign_links)
+            campaign_links.extend(article_campaign_links)
 
         logger.info(f"Got ({len(campaign_links)}) of campaign links.")
-
         return campaign_links
+
+    def _get_campaign_links_from_article(self, article_link: str) -> List[str]:
+        logger.info(f"Visiting {article_link} ...")
+        article_campaign_links = []
+        try:
+            const_timeout_in_seconds = 16
+            response = requests.get(article_link, timeout=const_timeout_in_seconds)
+            response.raise_for_status()
+        except requests.exceptions.ConnectionError:
+            logger.warning(f"Could not get the link: {article_link}")
+            return []
+        except requests.exceptions.Timeout:
+            logger.warning(f"Timeout when getting the link: {article_link}")
+            return []
+        except requests.exceptions.HTTPError as http_err:
+            logger.warning(f"HTTP error occurred when getting the link: {article_link} - {http_err}")
+            return []
+        except requests.exceptions.RequestException as req_err:
+            logger.warning(f"Request exception occurred when getting the link: {article_link} - {req_err}")
+            return []
+
+        inner_soup = BeautifulSoup(response.text, "html.parser")
+
+        for a_tag in inner_soup.find_all("a", href=True):
+            campaign_link = a_tag["href"]
+            if self.is_starting_with_campaign_url(campaign_link):
+                article_campaign_links.append(campaign_link)
+            else:
+                # Debugging info.
+                # Only log when it looks like a URL but not a campaign link.
+                const_site_inner_link_substring = "clien"
+                if campaign_link.find("http") != -1 and campaign_link.find(const_site_inner_link_substring) == -1:
+                    logger.info(f"Not a campaign link: {campaign_link}")
+
+        return article_campaign_links
